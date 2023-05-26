366be4cef17fd55051065cb6048823fc3db31362
Vyacheslav Ananev <vnva.dev@gmail.com>
2023-05-26 15:43:35 +0700

    feat(repo): typography configurated

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,7 +1,31 @@
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
 
 import '@/core/styles/index.scss';
 
+const montserratFont = localFont({
+  src: [
+    { path: '../core/assets/fonts/montserrat-bold.woff2', weight: '700', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-bold.woff', weight: '700', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-semi-bold.woff2', weight: '600', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-semi-bold.woff', weight: '600', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-medium.woff2', weight: '500', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-medium.woff', weight: '500', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-regular.woff2', weight: '400', style: 'normal' },
+    { path: '../core/assets/fonts/montserrat-regular.woff', weight: '400', style: 'normal' },
+  ],
+  display: 'swap',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style global jsx>{`
+        :root {
+          --montserrat-font: ${montserratFont.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
